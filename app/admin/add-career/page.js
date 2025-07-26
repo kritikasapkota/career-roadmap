f800e67cc959580c791c0ebf8374f5176a883dfe
kritikasapkota <kritikasapkota2023cse@sece.ac.in>
2025-07-26 09:58:09 +0530

    improved classname and styling

--- app/admin/add-career/page.js
+++ app/admin/add-career/page.js
@@ -3,12 +3,25 @@ import styles from './addcareer.module.css';
 export default function AddCareer() {
   return (
     <div className={styles.add}>
-      <h2>Add New Career</h2>
-      <form>
-        <input type="text" placeholder="Career Name" />
-        <textarea placeholder="Steps (comma separated)"></textarea>
-        <input type="text" placeholder="Image URL or path" />
-        <button type="submit">Add Career</button>
+      <h2 className={styles.heading}>Add New Career</h2>
+      <form className={styles.form}>
+        <input
+          type="text"
+          placeholder="Career Name"
+          className={styles.input}
+        />
+        <textarea
+          placeholder="Steps (comma separated)"
+          className={styles.textarea}
+        ></textarea>
+        <input
+          type="text"
+          placeholder="Image URL or path"
+          className={styles.input}
+        />
+        <button type="submit" className={styles.button}>
+          Add Career
+        </button>
       </form>
     </div>
   );
